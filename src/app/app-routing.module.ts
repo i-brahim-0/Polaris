3773c4ed70ebf4fbace4c6465d76af825b88d57b
gitconfig <him9100@gmail.com>
2024-07-06 03:16:41 +0300

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { ProductsComponent } from './products/products.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', canActivate: [AuthGuard], component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'products', component: ProductsComponent },
+  { path: 'products', canActivate: [AuthGuard], component: ProductsComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', component: NotFoundComponent },
 ];
